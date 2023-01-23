4ca395292011d271bce155b8d5f1f58c3de0238f
Dheeraj <gembali.venkata.dheeraj@gmail.com>
2023-01-24 00:31:30 +0530

    first commit

---
+++ Test1/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+      //  git config --global user.email "gembali.venkata.dheeraj@gmail.com"
+        System.out.println("Hello world!");
+    }
+}
